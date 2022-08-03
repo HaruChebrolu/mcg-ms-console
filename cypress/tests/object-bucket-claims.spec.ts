9c45acefa43f8fc45ba8044ea7fd08a869012410
Chebrolu Harika <hchebrol@redhat.com>
2022-08-03 22:39:36 +0530

    Create OBC in redhat-data-federation namespace
    Signed-off-by: Chebrolu Harika <hchebrol@redhat.com>

--- cypress/tests/object-bucket-claims.spec.ts
+++ cypress/tests/object-bucket-claims.spec.ts
@@ -1,4 +1,7 @@
+import { DATA_FEDERATION_NAMESPACE } from '../constants/common';
 import { BUCKET_CLAIM_NAME } from '../constants/tests';
+import { projectNameSpace } from '../views/common';
+import { createObjectBucketClaim, deleteObjectClaimResources } from '../views/object-bucket-claims';
 
 describe('Object Bucket Claims page', () => {
   before(() => {
@@ -12,28 +15,17 @@ describe('Object Bucket Claims page', () => {
   after(() => {
     cy.logout();
   });
-  it('creates an Object Bucket Claim', () => {
-    cy.intercept('/api/kubernetes/apis/storage.k8s.io/v1/storageclasses*').as(
-      'listStorageClasses'
-    );
-    cy.byTestID('item-create').click();
-    cy.wait('@listStorageClasses');
-    cy.byTestID('obc-name').type(BUCKET_CLAIM_NAME);
-    cy.byTestID('sc-dropdown-toggle').click();
-    cy.byTestID('sc-dropdown').find('li').first().click();
-    cy.byTestID('obc-create').click();
-    cy.byTestSelector('details-item-value__Name').should(
-      'contain',
-      BUCKET_CLAIM_NAME
-    );
-    cy.byTestID('status-text').should('contain', 'Bound');
+  it('creates an Object Bucket Claim outside of redhat-data-federation namespace', () => {
+    createObjectBucketClaim(BUCKET_CLAIM_NAME)
   });
 
   it('deletes an Object Bucket Claim', () => {
-    cy.byLegacyTestID(BUCKET_CLAIM_NAME).first().click();
-    cy.byTestID('details-actions').find('button').click();
-    cy.byTestID('details-actions').find('li').last().click();
-    cy.byTestID('delete-action').click();
-    cy.byLegacyTestID(BUCKET_CLAIM_NAME).should('not.exist');
+    deleteObjectClaimResources(BUCKET_CLAIM_NAME)
+  });
+
+  it('creates an Object Bucket Claim inside of redhat-data-federation namespace', () => {
+    projectNameSpace.selectOrCreateProject(DATA_FEDERATION_NAMESPACE);
+    createObjectBucketClaim(BUCKET_CLAIM_NAME)
+    deleteObjectClaimResources(BUCKET_CLAIM_NAME)
   });
 });
