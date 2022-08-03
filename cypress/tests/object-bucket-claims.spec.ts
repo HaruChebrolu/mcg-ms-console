03c1ee0d1dd1cbe2bf6c1557305838cc52f95687
Chebrolu Harika <hchebrol@redhat.com>
2022-08-03 22:40:55 +0530

    Create OBC in redhat-data-federation namespace
    Signed-off-by: Chebrolu Harika <hchebrol@redhat.com>

--- cypress/tests/object-bucket-claims.spec.ts
+++ cypress/tests/object-bucket-claims.spec.ts
@@ -1,7 +1,10 @@
 import { DATA_FEDERATION_NAMESPACE } from '../constants/common';
 import { BUCKET_CLAIM_NAME } from '../constants/tests';
 import { projectNameSpace } from '../views/common';
-import { createObjectBucketClaim, deleteObjectClaimResources } from '../views/object-bucket-claims';
+import {
+  createObjectBucketClaim,
+  deleteObjectClaimResources,
+} from '../views/object-bucket-claims';
 
 describe('Object Bucket Claims page', () => {
   before(() => {
@@ -16,16 +19,16 @@ describe('Object Bucket Claims page', () => {
     cy.logout();
   });
   it('creates an Object Bucket Claim outside of redhat-data-federation namespace', () => {
-    createObjectBucketClaim(BUCKET_CLAIM_NAME)
+    createObjectBucketClaim(BUCKET_CLAIM_NAME);
   });
 
   it('deletes an Object Bucket Claim', () => {
-    deleteObjectClaimResources(BUCKET_CLAIM_NAME)
+    deleteObjectClaimResources(BUCKET_CLAIM_NAME);
   });
 
   it('creates an Object Bucket Claim inside of redhat-data-federation namespace', () => {
     projectNameSpace.selectOrCreateProject(DATA_FEDERATION_NAMESPACE);
-    createObjectBucketClaim(BUCKET_CLAIM_NAME)
-    deleteObjectClaimResources(BUCKET_CLAIM_NAME)
+    createObjectBucketClaim(BUCKET_CLAIM_NAME);
+    deleteObjectClaimResources(BUCKET_CLAIM_NAME);
   });
 });
--- cypress/views/object-bucket-claims.ts
+++ cypress/views/object-bucket-claims.ts
@@ -1,26 +1,26 @@
 export const createObjectBucketClaim = (bucketClaimName: string) => {
-    cy.log(`creating object bucket claim`);
-    cy.intercept('/api/kubernetes/apis/storage.k8s.io/v1/storageclasses*').as(
-        'listStorageClasses'
-    );
-    cy.byTestID('item-create').click();
-    cy.wait('@listStorageClasses');
-    cy.log(`entering data source name as ${bucketClaimName}`);
-    cy.byTestID('obc-name').type(bucketClaimName);
-    cy.byTestID('sc-dropdown-toggle').click();
-    cy.byTestID('sc-dropdown').find('li').first().click();
-    cy.byTestID('obc-create').click();
-    cy.byTestSelector('details-item-value__Name').should(
-        'contain',
-        bucketClaimName
-    );
-    cy.byTestID('status-text').should('contain', 'Bound');
+  cy.log(`creating object bucket claim`);
+  cy.intercept('/api/kubernetes/apis/storage.k8s.io/v1/storageclasses*').as(
+    'listStorageClasses'
+  );
+  cy.byTestID('item-create').click();
+  cy.wait('@listStorageClasses');
+  cy.log(`entering data source name as ${bucketClaimName}`);
+  cy.byTestID('obc-name').type(bucketClaimName);
+  cy.byTestID('sc-dropdown-toggle').click();
+  cy.byTestID('sc-dropdown').find('li').first().click();
+  cy.byTestID('obc-create').click();
+  cy.byTestSelector('details-item-value__Name').should(
+    'contain',
+    bucketClaimName
+  );
+  cy.byTestID('status-text').should('contain', 'Bound');
 };
 
 export const deleteObjectClaimResources = (bucketClaimName: string) => {
-    cy.byLegacyTestID(bucketClaimName).first().click();
-    cy.byTestID('details-actions').find('button').click();
-    cy.byTestID('details-actions').find('li').last().click();
-    cy.byTestID('delete-action').click();
-    cy.byLegacyTestID(bucketClaimName).should('not.exist');
+  cy.byLegacyTestID(bucketClaimName).first().click();
+  cy.byTestID('details-actions').find('button').click();
+  cy.byTestID('details-actions').find('li').last().click();
+  cy.byTestID('delete-action').click();
+  cy.byLegacyTestID(bucketClaimName).should('not.exist');
 };
